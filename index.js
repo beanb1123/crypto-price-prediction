3c85c93605ccf78f61a0c9ab0ad490525b5f72be
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-04-02 15:24:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ binance.candlesticks(coinPair, interval, (error, ticks) => {
     let sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
     sleep(wait_time);
       
-    let real_price = await fetch('https://www.binance.us/api/v3/ticker/price?symbol=BNBUSDT').json();
+    let real_price = await (await fetch('https://www.binance.us/api/v3/ticker/price?symbol=BNBUSDT')).json();
     console.log(real_price);
 
   }
