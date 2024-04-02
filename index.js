a28df3543afbc4198e22d498dd15de0cb893a961
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-04-02 16:51:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ for (let i = 0; i < data.length - sequenceLength; i++) {
   const model = tf.sequential();
   model.add(tf.layers.lstm({
    units: sequenceLength, // Adjust units as needed
-   inputShape: [sequenceLength, 1], 
+   inputShape: [input, output], 
    returnSequences: false // Return single output for prediction
   }));
   model.add(tf.layers.dense({ units: 1 })); 
